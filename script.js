71bae51e6ffc70dffeab584421538564d85eda71
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-08-16 18:57:06 +0000

    jdjdid

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const lyricsInput = document.getElementById('lyrics-input');
     const singButton = document.getElementById('sing-button');
     const lyricsDisplay = document.getElementById('lyrics-display');
+
+    singButton.addEventListener('click', () => {
+        const lyrics = lyricsInput.value;
+        if (lyrics) {
+            sing(lyrics);
+        }
+    });
+
+    function sing(lyrics) {
+        lyricsDisplay.textContent = '';
+        petRock.classList.add('singing');
+
+        let i = 0;
+        const singInterval = setInterval(() => {
+            if (i < lyrics.length) {
+                lyricsDisplay.textContent += lyrics[i];
+                i++;
+            } else {
+                clearInterval(singInterval);
+                petRock.classList.remove('singing');
+            }
+        }, 100);
+    }
+});
