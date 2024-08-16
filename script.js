1312a00b70ce319b952e726fbab1371f431316c8
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-08-16 17:49:02 +0000

    mfdiie

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const petRock = document.getElementById('pet-rock');
+    const lyricsInput = document.getElementById('lyrics-input');
+    const singButton = document.getElementById('sing-button');
+    const lyricsDisplay = document.getElementById('lyrics-display');
